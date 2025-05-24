28275f25260102dc4b38724462c6f0b63615d38f
Brady <bmiller5137@gmail.com>
2025-05-23 21:38:41 -0500

    Rename FactBaseApplication to KnowledgeBaseApplication

--- src/main/java/com/knowledgebase/KnowledgeBaseApplication.java
+++ src/main/java/com/knowledgebase/KnowledgeBaseApplication.java
@@ -2,12 +2,8 @@ package com.knowledgebase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.knowledgebase", "com.yourpackage"})
-@EntityScan(basePackages = {"com.knowledgebase", "com.yourpackage"})
 public class KnowledgeBaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(KnowledgeBaseApplication.class, args);
