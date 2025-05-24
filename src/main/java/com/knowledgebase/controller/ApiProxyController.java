d961ce8cf6e7d83136bfeadc726d6ef84c9affaa
Brady <bmiller5137@gmail.com>
2025-05-23 21:23:42 -0500

    Fix Map import in ApiProxyController

--- src/main/java/com/knowledgebase/controller/ApiProxyController.java
+++ src/main/java/com/knowledgebase/controller/ApiProxyController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;  // Make sure this import is here
+
 @RestController
 @RequestMapping({"/api", ""})
 public class ApiProxyController {
